363a797e06d2a952a95cc1659e77ae968df54367
Alan <maximob1@htomai.com>
2026-06-05 08:10:29 -0600

    parte2

--- examen.java
+++ examen.java
@@ -7,12 +7,29 @@ public class examen{
         {80,85,78,90},
         {50,60,55,48}
     };
+
+    double promedio= resultado / 4;
+
    System.out.println( "📡 Analizando lecturas de humedad por sector... ");
    System.out.println("-----------------------------------------------------");
 
    System.out.println("Invernadero A "+ "(Sectores: "+ operacion(Humedad, 0) );
-   System.out.println(); 
-    }
+   System.out.println("Promedio Humedad: "+ promedio); 
+
+   if (promedio < 35){
+    System.out.println("Requiere riego de emergencia");
+
+    if (promedio >35 || promedio < 75) {
+        System.out.println("Estable y Optimizado");
+
+     if (promedio>75){
+        System.out.println("Execeso de agua");
+
+        }
+     }
+   }
+ }
+    
 
     public static int operacion(int [][] matriz, int fila){
     
@@ -20,9 +37,14 @@ public class examen{
     for(int i=0; i < matriz.length; i++){
     int resultado =+ matriz[fila][i];
 
-    promedio = resultado /4;
+    if (matriz[fila][i] < 30){
+        int cont =0;
+        cont =cont +1; 
+        System.out.println("Alerta de Sequía: Se detectaron "+ cont +" sectores en estado crítico (Humedad < 30%).");
 
-    return promedio;  
+    }
+
+    return resultado; 
     }
     }
 
